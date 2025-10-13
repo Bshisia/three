1e9c8ce1b23b2936a6f5081fa3f4934977960b75
Master <bigman@Bigman.Master>
2025-10-13 19:36:17 +0300

    feat: color the different sides of the cube by defining the indices of the cubes in the materials ap

--- main.js
+++ main.js
@@ -1,26 +1,28 @@
 import * as THREE from 'three';
 
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 1000);
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
 const renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
 
-const geometry = new THREE.BoxGeometry();
-const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-const cube = new THREE.Mesh(geometry, material);
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const materials = [
+    new THREE.MeshBasicMaterial({ color: 0xff0000 }),
+    new THREE.MeshBasicMaterial({ color: 0x00ff00 }),
+    new THREE.MeshBasicMaterial({ color: 0x0000ff }),
+    new THREE.MeshBasicMaterial({ color: 0xffff00 }),
+    new THREE.MeshBasicMaterial({ color: 0xff00ff }),
+    new THREE.MeshBasicMaterial({ color: 0x00ffff }),
+];
+const cube = new THREE.Mesh(geometry, materials);
 scene.add(cube);
-
 camera.position.z = 5;
 
 function animate() {
-    requestAnimationFrame(animate);
-    cube.rotation.y += 0.002;
-    cube.rotation.z += 0.002;
-    cube.scale.x = Math.sin(Date.now() * 0.002) + 2;
-    cube.scale.y = Math.cos(Date.now() * 0.002) + 2;
-    cube.position.x = Math.sin(Date.now() * 0.002);
-    cube.position.y = Math.cos(Date.now() * 0.002);
+    cube.rotation.x += 0.01;
+    cube.rotation.y += 0.01;
     renderer.render(scene, camera);
 }
-animate();
+renderer.setAnimationLoop(animate)
